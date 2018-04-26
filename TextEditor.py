5fd83255b75fa56b094e2e6777c711515e14c704
Lucas <Lucasgolden14@gmail.com>
2018-04-26 17:45:48 -0400

    Infinite loop when attempting to save before a file

--- TextEditor.py
+++ TextEditor.py
@@ -2,13 +2,22 @@
 
 from tkinter import filedialog
 from tkinter import *
+import io
 
 root=Tk("Text Editor")
 text = Text(root)
+check = 0
+check2 = 0
 #initializes the text editor
 def init():
     text.grid()
     root.mainloop()
+
+def button1_callback():
+    check += 1
+
+def button2_callback():
+    check2 += 1
 
 # saves the file as a .txt file
 def file_save():
@@ -19,10 +28,28 @@ def file_save():
     f.write(text2save)
     f.close()
 
+def check_save():
+    toplevel = Toplevel()
+    label1 = Label(toplevel, text="Would you like to save?", height=10, width=100)
+    label1.pack()
+    button3 = Button(toplevel, text = "Yes", command= button1_callback)
+    button4 = Button(toplevel,text = "No", command =  button2_callback)
+    button3.grid()
+    button4.grid()
+    if check > 0:
+        file_save()
+    else:
+        pass
+
 def file_open():
-    root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"), ("text files", "*.txt"),("all files","*.*")))
-    with open(root.filename, "r") as f:
-        Label(root, text=f.read()).pack()
+    check_save()
+    filename =  filedialog.askopenfilename(initialdir = "/",
+        title = "Select file",filetypes = (("jpeg files","*.jpg"),
+        ("text files", "*.txt"),("all files","*.*")))
+    with open(filename) as file:
+        data = file.read()
+        for line in data:
+            text.insert(INSERT, line)
 
 button=Button(root, text="Save", command=file_save) 
 button2 = Button(root, text = "Open File", command = file_open)
